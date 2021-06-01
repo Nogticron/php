c12d6354c955f652253f2c13802214c027a56622
noguchi-da-ts <noguchi_d@tenso.com>
2021-06-01 20:40:31 +0900

    mondai 2

---
+++ 5programs/mondai2.php
@@ -0,0 +1,31 @@
+<?php
+
+function cross_array($list_a, $list_b) {
+
+  if( count($list_a) > count($list_b) ){
+    $max_list_length = count($list_a);
+  } else {
+    $max_list_length = count($list_b);
+  }
+
+  $crossed_list = array();
+  for($i = 0; $i < $max_list_length; $i++) {
+    array_push($crossed_list, $list_a[$i], $list_b[$i]);
+  }
+
+  return array_filter($crossed_list);
+}
+
+$list_x = array('a', 'b', 'c');
+$list_y = array(1, 2, 3);
+
+echo "交互に結合!\n";
+print_r(cross_array($list_x, $list_y));
+
+$list_x = array('a', 'b', 'c');
+$list_y = array(1, 2, 3, 4, 5);
+
+echo "\n\n長さの違う配列を結合!\n";
+print_r(cross_array($list_x, $list_y));
+
+?>
